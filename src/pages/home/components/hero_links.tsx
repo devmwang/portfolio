f5f0634107137af1fde738a8eac5f92fb14922cd
Michael Wang <41971562+devmwang@users.noreply.github.com>
2022-06-03 18:22:54 -0700

    Updated home hero components to transition in links individually

--- src/pages/home/components/hero_links.tsx
+++ src/pages/home/components/hero_links.tsx
@@ -6,30 +6,36 @@ type LinkDetails = {
     description: string;
     link: string;
     imgSrc: any;
+    revealDelay: number;
 }
 
 interface ComponentProps {
     linkList: LinkDetails[];
-    revealDelay: number;
 }
 
 const HeroLinks = (props: ComponentProps) => {
-    const [isVisible, setVisible] = useState(false);
-    
+    const [isVisible, setVisible] = useState(new Array(props.linkList.length).fill(false));
+
     // Runs on component mount
     useEffect(() => {
-        // Reveal name text after delay
-        setTimeout(() => {
-            setVisible(true);
-        }, props.revealDelay)
+        // For each link, reveal after specified delay
+        for (let i=0; i<props.linkList.length; i++) {
+            setTimeout(() => {
+                setVisible((currentValues: any) => {
+                    return currentValues.map((value: boolean, index: number) => {
+                        return index == i ? true : value;
+                    })
+                })
+            }, props.linkList[i].revealDelay)
+        }
     }, []);
     
     // Link List Items
-    const listItems = props.linkList.map((linkItem) => {
+    const listItems = props.linkList.map((linkItem, index) => {
         const { description, link, imgSrc } = linkItem;
         
         return (
-            <a key={link} href={link} target="_blank" rel="noopener noreferrer">
+            <a key={link} href={link} target="_blank" rel="noopener noreferrer" className={isVisible[index] ? styles.visible : styles.hidden}>
                 <img src={imgSrc} alt={description} className={styles.linkItem} />
             </a>
             )
@@ -39,9 +45,7 @@ const HeroLinks = (props: ComponentProps) => {
         <React.Fragment>
             <div className={styles.linksContainer}>
                 <div className={styles.links}>
-                    <div className={isVisible ? styles.visible : styles.hidden}>
-                        {listItems}
-                    </div>
+                    {listItems}
                 </div>
             </div>
         </React.Fragment>
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -11,8 +11,8 @@ import DownArrowGlyph from "@assets/icons/DownArrowGlyph.svg";
 
 const Home = () => {
     const links = [
-        { description: "GitHub", link: "https://github.com/devmwang/", imgSrc: GithubLogo },
-        { description: "Email", link: "mailto:devmwang@icloud.com", imgSrc: EmailIcon }
+        { description: "GitHub", link: "https://github.com/devmwang/", imgSrc: GithubLogo, revealDelay: 800 },
+        { description: "Email", link: "mailto:devmwang@icloud.com", imgSrc: EmailIcon, revealDelay: 1000 }
     ]
 
     return (
@@ -20,7 +20,7 @@ const Home = () => {
             <div className={styles.hero}>
                 <div className={styles.heroItems}>
                     <HeroName fullName="Michael Wang" revealDelay={500} />
-                    <HeroLinks linkList={links} revealDelay={750} />
+                    <HeroLinks linkList={links} />
                 </div>
             </div>
         </React.Fragment>
